eaf072fd9188d7f264d5c4b76ca59213b822072c
Yurui-Feng <yurifeng@bu.edu>
2023-01-27 18:53:17 -0800

    added working sentiment analysis function

--- R/sentiment_analysis.R
+++ R/sentiment_analysis.R
@@ -1,3 +1,5 @@
+library(syuzhet)
+library(dplyr)
 #' Labelling each row in a given column of tweets/text with positive, negative or neutral sentiment
 #'
 #' @param df dataframe after pre-processing
@@ -7,16 +9,41 @@
 #' @export
 #'
 #' @examples
-#' x <- get_sentiment_result(df, "text")
+#' x <- sentiment_labeler(df, "text")
+#'
+sentiment_labeler <- function(df, col) {
+  df$score <- syuzhet::get_nrc_sentiment(df[, col])
+  df <- df |>
+    mutate(sentiment =
+             ifelse(score$positive > score$negative,
+                    "positive",
+             ifelse(score$positive < score$negative,
+                    "negative",
+             ifelse(score$positive == score$negative,
+                    "neutral",
+                    "ERROR"))))
+  df <- df |>
+    select(-score)
+  return(df)
+}
 
 
 #' Count the propotion of different sentiment tweets in a labelled sentiment dataframe
 #'
 #' @param df A dataframe after pre-processing
-#' @param boolean A boolean value if True: returns the proportion; otherwise, return the counts
+#' @param proportion A boolean value if True: returns the proportion; otherwise, return the counts
 #'
 #' @return A dictionary which calculates the proportion of three sentiments of tweets
 #' @export
 #'
 #' @examples
 #' x <- count_tweets(labelled_df)
+count_tweets <- function(df, proportion = TRUE) {
+  if (proportion) {
+    sentiment_counts <- prop.table(table(df$sentiment))
+  } else {
+    sentiment_counts <- table(df$sentiment)
+  }
+
+  return(as.list(sentiment_counts))
+}
