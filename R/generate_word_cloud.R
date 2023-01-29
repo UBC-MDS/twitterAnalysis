9d3bcd1ff81da5b213afc2d66d14c5fd7da1fdcd
Renzo Wijngaarden <renzorenzo55@gmail.com>
2023-01-29 00:38:10 -0800

    added wordcloud functions and tests

--- R/generate_word_cloud.R
+++ R/generate_word_cloud.R
@@ -1,23 +1,75 @@
-#' Get top n words from a specific sentiment dataframe
+library(tm)
+library(wordcloud)
+
+#' Create a clean corpus of all the used words in a users tweets.
 #'
-#' @param df A dataframe after sentiment_analysis
-#' @param n An integer shows how many number of top existing words in dataframe with tweets msg column
+#' @param df A dataframe containing a users tweets that has been preprocessed.
 #'
-#' @return A dictionary with key as word that has highest frequency and value as how many number it show up
+#' @return A VCorpus of all the words used disregarding stopwords and punctuation.
 #' @export
 #'
 #' @examples
-#' x <- get_top_n_word(df, 5)
+#' \dontrun{
+#'   docs <- clean_tweets(df)
+#' }
 
+clean_tweets <- function(df) {
 
+  text <- df$text_clean
+  docs <- tm::VCorpus(tm::VectorSource(text))
+  docs <- docs %>%
+    tm::tm_map(tm::removeNumbers) %>%
+    tm::tm_map(tm::removePunctuation) %>%
+    tm::tm_map(tm::stripWhitespace)
+  docs <- tm::tm_map(docs, tm::content_transformer(tolower))
+  docs <- tm::tm_map(docs, tm::removeWords, tm::stopwords("english"))
 
-#' Create Wordcloud for each user's tweets
+  return(docs)
+}
+
+#' Create a dataframe with all the words in a users tweets and how often they appear.
 #'
-#' @param text A str contains tweets messages
-#' @param output_path The path that the newly generated png should located at
+#' @param docs A VCorpus of words used in a users tweets.
 #'
-#' @return A png which contains the picture of the wordcount
+#' @return A dataframe with all the words in the tweets and how often they appear.
 #' @export
 #'
 #' @examples
-#' x <- create_wordcloud('today is bad day', /output)
+#' \dontrun{
+#'   df2 <- count_words(docs)
+#' }
+
+count_words <- function(docs) {
+
+  dtm <- as.matrix(tm::TermDocumentMatrix(docs))
+  words <- sort(rowSums(dtm), decreasing = TRUE)
+  df_words <- data.frame(word = names(words), freq = words)
+
+  return(df_words)
+}
+
+#' Create a wordcloud of a users tweets.
+#'
+#' @param df A dataframe with all the words in the tweets and how often they appear.
+#'
+#' @return A wordcloud of a users tweets.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   create_wordcloud(df2)
+#' }
+
+create_wordcloud <- function(df) {
+
+  set.seed(524)
+
+  wc <- wordcloud(
+    words = df$word,
+    freq = df$freq,
+    min.freq = 2,
+    max.words = 200,
+    random.order = FALSE,
+    rot.per = 0.35,
+  )
+}
