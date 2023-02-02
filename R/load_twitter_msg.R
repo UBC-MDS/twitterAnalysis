0348308a67655742918462892eacc5c0c0257df6
Andy Wang <wang64192664@gmail.com>
2023-02-01 23:18:13 -0800

    try

--- R/load_twitter_msg.R
+++ R/load_twitter_msg.R
@@ -30,42 +30,16 @@ user_info <- function(consumer_key_input, consumer_secret_input, access_token_in
 
 #' Load dataframe which contains specific user and return as a dataframe with total tweets
 #'
-#' @param user A str contains user_name
-#' @param limit An integer shows how many tweets user want to return
-#'
-#' @return A dataframe
-#' @export
-#'
-#' @examples
-#' \dontrun{
-#' x <- load_twitter_by_user('nice', 50, user)
-#' }
-load_twitter_by_keywords <- function(user_name, limit, user_info) {
-  consumer_key <- user_info$consumer_key
-  consumer_secret <- user_info$consumer_secret
-  access_token <- user_info$access_token
-  access_secret <- user_info$access_token_secret
-
-  setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
-  res = searchTwitter(user_name, n=limit)
-  res_df <- twListToDF(res)
-
-  final_df <- res_df |> select(screenName,id, created, favoriteCount, retweetCount, text)
-
-}
-
-
-#' Load dataframe which contains specific user and return as a dataframe with total tweets
-#'
 #' @param key A str contains tweets keyword
 #' @param limit An integer shows how many tweets user want to return
+#' @param user_info A list contains all user credentials
 #'
 #' @return A dataframe contains all tweets contains key words you passed
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' x <- load_twitter_by_keywords('2022', 100)
+#' x <- load_twitter_by_keywords('2022', 100, user_info)
 #' }
 load_twitter_by_user <- function(keyword, limit, user_info) {
   consumer_key <- user_info$consumer_key
