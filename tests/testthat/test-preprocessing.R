b047d605d9fce1aca38b80d7f5b842b6168e093a
Renzo Wijngaarden <renzorenzo55@gmail.com>
2023-02-03 16:15:34 -0800

    Updated test files with documentation

--- tests/testthat/test-preprocessing.R
+++ tests/testthat/test-preprocessing.R
@@ -1,84 +1,100 @@
+# Define supporting variables
 
-potus_df <- data.frame(User =c('POTUS', 'POTUS', 'POTUS', 'POTUS', 'POTUS', 'POTUS', 'POTUS', 'POTUS'),
-                 text = c('In America, we go forward when we go together.',
-                          "Like Dr. King, I know that there is nothing beyond this nation's capacity. \n\nWe will fulfill the promise of America for all Americans. https://t.co/toXTEOtIVj",
-                          "Let us be guided by Dr. King's light today. And by the charge of Scripture:\n\n“Let us never grow weary in doing what is right – for if we do not give up we will reap our harvest in due time.”",
-                          "My Administration laid the foundation for a stronger, more resilient, and more equitable economy for decades to come.\n\nTwo years in – it's clear our economic plan is working. https://t.co/OtfL4abi2j",
-                          'This is a time for choosing. \n\nWill we choose democracy over autocracy?\nCommunity over chaos?\nLove over hate?\n\nThese are questions of our time that I ran for president to help answer. And of which Dr. King’s life and legacy will guide us forward.',
-                          'Tune in as I deliver remarks at the National Action Network Martin Luther King, Jr. Day Breakfast. https://t.co/q6IH0MDAvk',
-                          'In the Oval Office is a bust of Dr. King.\n \nI often think of the question Dr. King asked us all those years ago: “where do we go from here?”\n \nMy message today is that we go forward when we go together.',
-                          'Today, we honor the Reverend Dr. Martin Luther King, Jr. by continuing his unfinished work to redeem the soul of America. https://t.co/yJ4rmLyJyt')
+potus_df <- data.frame(
+  User = c("POTUS", "POTUS", "POTUS", "POTUS", "POTUS", "POTUS", "POTUS", "POTUS"),
+  text = c(
+    "In America, we go forward when we go together.",
+    "Like Dr. King, I know that there is nothing beyond this nation's capacity. \n\nWe will fulfill the promise of America for all Americans. https://t.co/toXTEOtIVj",
+    "Let us be guided by Dr. King's light today. And by the charge of Scripture:\n\n“Let us never grow weary in doing what is right – for if we do not give up we will reap our harvest in due time.”",
+    "My Administration laid the foundation for a stronger, more resilient, and more equitable economy for decades to come.\n\nTwo years in – it's clear our economic plan is working. https://t.co/OtfL4abi2j",
+    "This is a time for choosing. \n\nWill we choose democracy over autocracy?\nCommunity over chaos?\nLove over hate?\n\nThese are questions of our time that I ran for president to help answer. And of which Dr. King’s life and legacy will guide us forward.",
+    "Tune in as I deliver remarks at the National Action Network Martin Luther King, Jr. Day Breakfast. https://t.co/q6IH0MDAvk",
+    "In the Oval Office is a bust of Dr. King.\n \nI often think of the question Dr. King asked us all those years ago: “where do we go from here?”\n \nMy message today is that we go forward when we go together.",
+    "Today, we honor the Reverend Dr. Martin Luther King, Jr. by continuing his unfinished work to redeem the soul of America. https://t.co/yJ4rmLyJyt"
+  )
 )
 
+# Tests
 
-test_that("removes rows with FAV and RT", {
-  df <- data.frame( User = c('POTUS', 'POTUS', 'POTUS'),
-                    text = c('RT tweet gets removed', 'FAV tweet gets removed', 'tweet passes')
-                    )
-  df_exp <- data.frame(row.names = c(as.integer(3)),
-                       User = c('POTUS'),
-                       text = c('tweet passes'),
-                       text_clean = c('tweet passes')
-                       )
+test_that("Removes rows with FAV and RT.", {
+  df <- data.frame(
+    User = c("POTUS", "POTUS", "POTUS"),
+    text = c("RT tweet gets removed", "FAV tweet gets removed", "tweet passes")
+  )
+  df_exp <- data.frame(
+    row.names = c(as.integer(3)),
+    User = c("POTUS"),
+    text = c("tweet passes"),
+    text_clean = c("tweet passes")
+  )
+
+  expect_identical(generalPreprocessing(df), df_exp)
+})
+
+
+
+test_that("Remove URLs, mentions, numbers.", {
+  df <- data.frame(
+    User = c("POTUS", "POTUS", "POTUS", "POTUS"),
+    text = c(
+      "tweet url https://t.co/q6IH0MDAvk",
+      "tweet @POTUS mention",
+      "tweet numbers 12345",
+      "tweet https://t.co/yJ4rmLyJyt 8234 @mention, url, number"
+    )
+  )
+  df_exp <- data.frame(
+    User = c("POTUS", "POTUS", "POTUS", "POTUS"),
+    text = c(
+      "tweet url https://t.co/q6IH0MDAvk",
+      "tweet @POTUS mention",
+      "tweet numbers 12345",
+      "tweet https://t.co/yJ4rmLyJyt 8234 @mention, url, number"
+    ),
+    text_clean = c(
+      "tweet url ",
+      "tweet  mention",
+      "tweet numbers ",
+      "tweet   , url, number"
+    )
+  )
 
   expect_identical(generalPreprocessing(df), df_exp)
 })
 
 
-
-test_that("remove URLs, mentions, numbers", {
-  df <- data.frame(User = c('POTUS', 'POTUS', 'POTUS', 'POTUS'),
-                   text = c('tweet url https://t.co/q6IH0MDAvk',
-                            'tweet @POTUS mention',
-                            'tweet numbers 12345',
-                            'tweet https://t.co/yJ4rmLyJyt 8234 @mention, url, number')
-  )
-  df_exp <- data.frame(User = c('POTUS', 'POTUS', 'POTUS', 'POTUS'),
-                       text = c('tweet url https://t.co/q6IH0MDAvk',
-                                'tweet @POTUS mention',
-                                'tweet numbers 12345',
-                                'tweet https://t.co/yJ4rmLyJyt 8234 @mention, url, number'),
-                       text_clean = c('tweet url ',
-                                'tweet  mention',
-                                'tweet numbers ',
-                                'tweet   , url, number')
-  )
-
-  expect_identical(generalPreprocessing(df), df_exp)
-  })
-
-
-test_that("shape", {
-  expect_equal(dim(generalPreprocessing(potus_df)), as.integer(c(8,3)))
+test_that("Dimension of output", {
+  expect_equal(dim(generalPreprocessing(potus_df)), as.integer(c(8, 3)))
 })
 
 
-test_that("shape", {
-  expect_true('text_clean' %in% colnames(generalPreprocessing(potus_df)))
+test_that("Existance of text_clean column in output", {
+  expect_true("text_clean" %in% colnames(generalPreprocessing(potus_df)))
 })
 
-test_that("potus_df",{
-  potus_df_exp <- data.frame(User =c('POTUS', 'POTUS', 'POTUS', 'POTUS', 'POTUS', 'POTUS', 'POTUS', 'POTUS'),
-                         text = c('In America, we go forward when we go together.',
-                                  "Like Dr. King, I know that there is nothing beyond this nation's capacity. \n\nWe will fulfill the promise of America for all Americans. https://t.co/toXTEOtIVj",
-                                  "Let us be guided by Dr. King's light today. And by the charge of Scripture:\n\n“Let us never grow weary in doing what is right – for if we do not give up we will reap our harvest in due time.”",
-                                  "My Administration laid the foundation for a stronger, more resilient, and more equitable economy for decades to come.\n\nTwo years in – it's clear our economic plan is working. https://t.co/OtfL4abi2j",
-                                  'This is a time for choosing. \n\nWill we choose democracy over autocracy?\nCommunity over chaos?\nLove over hate?\n\nThese are questions of our time that I ran for president to help answer. And of which Dr. King’s life and legacy will guide us forward.',
-                                  'Tune in as I deliver remarks at the National Action Network Martin Luther King, Jr. Day Breakfast. https://t.co/q6IH0MDAvk',
-                                  'In the Oval Office is a bust of Dr. King.\n \nI often think of the question Dr. King asked us all those years ago: “where do we go from here?”\n \nMy message today is that we go forward when we go together.',
-                                  'Today, we honor the Reverend Dr. Martin Luther King, Jr. by continuing his unfinished work to redeem the soul of America. https://t.co/yJ4rmLyJyt'),
-                         text_clean = c('In America, we go forward when we go together.',
-                                  "Like Dr. King, I know that there is nothing beyond this nation's capacity. \n\nWe will fulfill the promise of America for all Americans. ",
-                                  "Let us be guided by Dr. King's light today. And by the charge of Scripture:\n\n“Let us never grow weary in doing what is right – for if we do not give up we will reap our harvest in due time.”",
-                                  "My Administration laid the foundation for a stronger, more resilient, and more equitable economy for decades to come.\n\nTwo years in – it's clear our economic plan is working. ",
-                                  'This is a time for choosing. \n\nWill we choose democracy over autocracy?\nCommunity over chaos?\nLove over hate?\n\nThese are questions of our time that I ran for president to help answer. And of which Dr. King’s life and legacy will guide us forward.',
-                                  'Tune in as I deliver remarks at the National Action Network Martin Luther King, Jr. Day Breakfast. ',
-                                  'In the Oval Office is a bust of Dr. King.\n \nI often think of the question Dr. King asked us all those years ago: “where do we go from here?”\n \nMy message today is that we go forward when we go together.',
-                                  'Today, we honor the Reverend Dr. Martin Luther King, Jr. by continuing his unfinished work to redeem the soul of America. ')
-
-                           )
+test_that("Preprocessing of @POTUS tweets", {
+  potus_df_exp <- data.frame(
+    User = c("POTUS", "POTUS", "POTUS", "POTUS", "POTUS", "POTUS", "POTUS", "POTUS"),
+    text = c(
+      "In America, we go forward when we go together.",
+      "Like Dr. King, I know that there is nothing beyond this nation's capacity. \n\nWe will fulfill the promise of America for all Americans. https://t.co/toXTEOtIVj",
+      "Let us be guided by Dr. King's light today. And by the charge of Scripture:\n\n“Let us never grow weary in doing what is right – for if we do not give up we will reap our harvest in due time.”",
+      "My Administration laid the foundation for a stronger, more resilient, and more equitable economy for decades to come.\n\nTwo years in – it's clear our economic plan is working. https://t.co/OtfL4abi2j",
+      "This is a time for choosing. \n\nWill we choose democracy over autocracy?\nCommunity over chaos?\nLove over hate?\n\nThese are questions of our time that I ran for president to help answer. And of which Dr. King’s life and legacy will guide us forward.",
+      "Tune in as I deliver remarks at the National Action Network Martin Luther King, Jr. Day Breakfast. https://t.co/q6IH0MDAvk",
+      "In the Oval Office is a bust of Dr. King.\n \nI often think of the question Dr. King asked us all those years ago: “where do we go from here?”\n \nMy message today is that we go forward when we go together.",
+      "Today, we honor the Reverend Dr. Martin Luther King, Jr. by continuing his unfinished work to redeem the soul of America. https://t.co/yJ4rmLyJyt"
+    ),
+    text_clean = c(
+      "In America, we go forward when we go together.",
+      "Like Dr. King, I know that there is nothing beyond this nation's capacity. \n\nWe will fulfill the promise of America for all Americans. ",
+      "Let us be guided by Dr. King's light today. And by the charge of Scripture:\n\n“Let us never grow weary in doing what is right – for if we do not give up we will reap our harvest in due time.”",
+      "My Administration laid the foundation for a stronger, more resilient, and more equitable economy for decades to come.\n\nTwo years in – it's clear our economic plan is working. ",
+      "This is a time for choosing. \n\nWill we choose democracy over autocracy?\nCommunity over chaos?\nLove over hate?\n\nThese are questions of our time that I ran for president to help answer. And of which Dr. King’s life and legacy will guide us forward.",
+      "Tune in as I deliver remarks at the National Action Network Martin Luther King, Jr. Day Breakfast. ",
+      "In the Oval Office is a bust of Dr. King.\n \nI often think of the question Dr. King asked us all those years ago: “where do we go from here?”\n \nMy message today is that we go forward when we go together.",
+      "Today, we honor the Reverend Dr. Martin Luther King, Jr. by continuing his unfinished work to redeem the soul of America. "
+    )
+  )
   expect_identical(generalPreprocessing(potus_df), potus_df_exp)
-
-
-
 })
--- tests/testthat/test-sentiment_analysis.R
+++ tests/testthat/test-sentiment_analysis.R
@@ -1,45 +1,43 @@
+# Define supporting variables
+
 df <- data.frame(
   User = c("POTUS", "POTUS", "POTUS", "POTUS", "POTUS", "POTUS", "POTUS", "POTUS"),
   text = c(
-    "Jill and I send our deepest condolences to the families of all those killed in the tragic helicopter crash in Ukraine this morning – including Minister of Internal Affairs Monastyrskyy, a reformer and patriot. We grieve with all those who are mourning this heartbreaking tragedy.
-",
-    "An economy where a millionaire pays less in taxes than an educator or firefighter is an economy that doesn't work.   Our Administration's work to build a fairer tax code begins to right that wrong. https://t.co/hzxSc97kWl
-",
-    "Small business boom. I like the sound of that. Let's keep this momentum going, Administrator. https://t.co/J35qChJ0sv
-",
-    "Since I took office, over 10 million new small businesses have applied to set up shop.   That’s what it looks like to build an economy that looks out from Scranton — instead of looking down from Wall Street.
-",
+    "Jill and I send our deepest condolences to the families of all those killed in the tragic helicopter crash in Ukraine this morning – including Minister of Internal Affairs Monastyrskyy, a reformer and patriot. We grieve with all those who are mourning this heartbreaking tragedy.",
+    "An economy where a millionaire pays less in taxes than an educator or firefighter is an economy that doesn't work.   Our Administration's work to build a fairer tax code begins to right that wrong. https://t.co/hzxSc97kWl",
+    "Small business boom. I like the sound of that. Let's keep this momentum going, Administrator. https://t.co/J35qChJ0sv",
+    "Since I took office, over 10 million new small businesses have applied to set up shop.   That’s what it looks like to build an economy that looks out from Scranton — instead of looking down from Wall Street.",
     "House Republicans have prioritized protecting rich tax cheats, allowing them to pay less in taxes than middle-class Americans. That's called trickle-down economics.  Let me tell you a secret: it doesn't work.",
-    "Ordered a bacon cheeseburger from a local restaurant yesterday to celebrate over 10 million new small business applications during my presidency.   I may’ve caught Brittany off guard. https://t.co/rP8i8a0ny0
-",
+    "Ordered a bacon cheeseburger from a local restaurant yesterday to celebrate over 10 million new small business applications during my presidency.   I may’ve caught Brittany off guard. https://t.co/rP8i8a0ny0",
     "Today we learned the inflation rate for producers is at its lowest level since March 2021, leading to a decline in food and energy prices for working families.   Just the latest evidence that we're making progress tackling inflation. We're not letting up.",
-    "Folks, the law is on our side of the student debt relief fight – we’re not giving up. Hang in there and keep the faith. https://t.co/CQZTd7yvWj
-"
+    "Folks, the law is on our side of the student debt relief fight – we’re not giving up. Hang in there and keep the faith. https://t.co/CQZTd7yvWj"
   )
 )
 
-test_that("shape of the dataframe is correct", {
+# Tests
+
+test_that("Dimension of output", {
   labeled_df <- sentiment_labeler(df, "text")
   expect_equal(dim(labeled_df), c(8, 3))
 })
 
-test_that("sentiment column is present in dataframe", {
+test_that("Existance of sentiment column in output", {
   labeled_df <- sentiment_labeler(df, "text")
   expect_true("sentiment" %in% colnames(labeled_df))
 })
 
-test_that("only 3 unique sentiments are present", {
+test_that("Three distinct types of sentiment", {
   labeled_df <- sentiment_labeler(df, "text")
   expect_equal(length(unique(labeled_df$sentiment)), 3)
 })
 
-test_that("count_tweets() returns a list", {
+test_that("Type of output is list", {
   labeled_df <- sentiment_labeler(df, "text")
   count_dict <- count_tweets(labeled_df)
   expect_true(is.list(count_dict))
 })
 
-test_that("count_tweets() returns correct proportions", {
+test_that("Sentiment analysis proportion is correct", {
   labeled_df <- sentiment_labeler(df, "text")
   count_dict <- count_tweets(labeled_df)
   expect_equal(count_dict, list(negative = 0.25, neutral = 0.375, positive = 0.375))
